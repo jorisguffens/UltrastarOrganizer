b1c24d97b7b9a2bd180b5ac1b9af4ac2d5f55e08
Joris Guffens <joris.guffens@outlook.com>
2023-12-25 01:31:21 +0100

    tracklist can now also just print the list

--- src/main/java/be/jorisg/ultrastarorganizer/commands/tracklist/TracklistCommand.java
+++ src/main/java/be/jorisg/ultrastarorganizer/commands/tracklist/TracklistCommand.java
@@ -4,28 +4,33 @@ import be.jorisg.ultrastarorganizer.UltrastarOrganizer;
 import picocli.CommandLine;
 
 import java.io.File;
-import java.nio.file.FileSystems;
+import java.util.Arrays;
 
 @CommandLine.Command(name = "tracklist",
         description = "Create a document with a list of all songs.")
 public class TracklistCommand implements Runnable {
-
-    @CommandLine.Option(names = {"-t", "--type"}, description = "Output file type",
-            required = true, type = TracklistType.class)
-    private TracklistType type;
 
     @CommandLine.Option(names = {"--output"}, description = "Output directory/file")
     private File outputFile;
 
     @Override
     public void run() {
-        if ( outputFile == null ) {
-            outputFile = new File(UltrastarOrganizer.workDir, "tracklist." + type.name().toLowerCase());
-        } else if ( outputFile.isDirectory() ) {
-            outputFile = new File(outputFile, "tracklist." + type.name().toLowerCase());
-        }
-        
         UltrastarOrganizer.refresh();
+
+        if ( outputFile == null ) {
+            UltrastarOrganizer.library().tracks().forEach(track -> UltrastarOrganizer.out.println(CommandLine.Help.Ansi.AUTO.string(
+                    "@|cyan " + track.title() + " |@")));
+            return;
+        }
+
+        TracklistType type = Arrays.stream(TracklistType.values())
+                .filter(t -> outputFile.getName().toLowerCase().endsWith("." + t.name().toLowerCase()))
+                .findFirst().orElse(null);
+        if (type == null) {
+            UltrastarOrganizer.out.println(CommandLine.Help.Ansi.AUTO.string(
+                    "@|cyan Unsupported output format, valid options are: csv, odt. |@"));
+            return;
+        }
 
         try {
             if (!outputFile.exists()) {
